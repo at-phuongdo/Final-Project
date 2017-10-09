d5a37cb7bff63813147d50cc73f6f27e645647e4
at-VanTu <letu710@gmail.com>
2017-10-09 11:22:27 +0700

    update feature upload image

--- shop_online_admin/app/controllers/users_controller.rb
+++ shop_online_admin/app/controllers/users_controller.rb
@@ -45,6 +45,10 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
+      if params[:user][:avatar]
+        res = Cloudinary::Uploader.upload(params[:user][:avatar])
+        params[:user][:avatar] = res['url']
+      end
       if @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
